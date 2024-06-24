71f3e5f63909f7dcd02069edc919ebc52aaaaaf9
ngwenjeursular <ngwenjeursular@gmail.com>
2024-06-24 22:00:40 +0000

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars, no-shadow
     const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
     const task2 = false;
   }
 
